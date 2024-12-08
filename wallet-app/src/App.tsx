b1bd8068507c0203f307f2fb04cbe21f9951a1a5
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-12-08 06:26:27 -0300

    Aula 04

---
+++ wallet-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome to React Native!</Text>
+    </View>
+  );
+};
+
+export default App;
