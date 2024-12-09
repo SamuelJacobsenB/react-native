7a250d0c8edb6c4b855d5f0f43a39da9f49a9a52
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-12-09 17:59:21 -0300

    Criando páginas

---
+++ curso-rn/src/app/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Bem vindo à tela inicial</Text>
+    </View>
+  );
+};
+
+export default Home;
